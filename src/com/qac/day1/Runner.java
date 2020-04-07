1b2b4bf2f5e65e28aafb1978765845ea6a244cb0
Ashley <ashley.ritchie@academytrainee.com>
2020-04-07 16:33:10 +0100

    Java week 1 files added

---
+++ src/com/qac/day1/Runner.java
@@ -0,0 +1,20 @@
+package com.qac.day1;
+
+public class Runner {
+	/*
+	 * This is a multiline comment
+	 */
+	public static void main(String args[]) {
+		int age = 23; // age variable
+		int height = 176;
+		String homeAdd = "123 example road, London, m50 3sa";
+		Boolean bool = true;
+
+		System.out.println("Hello World");
+		System.out.println("I am " + age);
+		System.out.println("I am " + height + "cm");
+		System.out.println("My home address is: " + homeAdd);
+		System.out.println("The value of bool is: " + bool);
+	}
+
+}
